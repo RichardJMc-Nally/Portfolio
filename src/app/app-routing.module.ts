67b44b2e46c07d1e8265ab8a2c1a5f90fce6a75e
Richard <richardmcn2304@gmail.com>
2024-09-21 21:48:12 +0200

    Setting Up Basic Layout
    + Added Home Component
    + Added New Favicon
    + Updated Default Routing
    + Updated Some Packages
    - Removed Loading Portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '' }, // Default route
+  // { path: '**', redirectTo: '', pathMatch: 'full' }, // Default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
