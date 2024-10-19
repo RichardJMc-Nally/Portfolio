3cb4d96be42a841b357e63a50e4e48846e838974
Richard <richardmcn2304@gmail.com>
2024-10-19 18:21:37 +0200

    More Progress
    + Favicon Updated
    + Angular Material Added
    + Update Global Background SVG
    + Changed GitHub SVG Icon
    + Split Up Sections Into Individual Components
    | About Component Added
    | Projects Component Added
    | CV Component Added
    | Contact Component Added
    - Removed All SASS from app.component.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,34 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { CvComponent } from './cv/cv.component';
+import { ContactComponent } from './contact/contact.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    CvComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatTooltipModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
